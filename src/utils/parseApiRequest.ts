b604e5ee438ba17045c38741b8e5e6b7f3c67835
domin-mnd <dominnya@gmail.com>
2025-11-17 14:08:25 +0400

    fix(parseApiRequest): dedupe url slash

--- src/utils/parseApiRequest.ts
+++ src/utils/parseApiRequest.ts
@@ -15,7 +15,7 @@ interface ApiRequest extends ApiResponse {
 /** Parse ApiRequest to a Request object. */
 export function parseApiRequest(request: ApiRequest): Request {
   // Parse query parameters
-  const url = new URL(request.baseUrl + request.path);
+  const url = new URL(`${request.baseUrl.replace(/\/+$/g, '')}/${request.path.replace(/^\/+/g, '')}`);
   if (request.queryParams) {
     for (const [key, value] of Object.entries(request.queryParams)) {
       url.searchParams.append(key, String(value));
